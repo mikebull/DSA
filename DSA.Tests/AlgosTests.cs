aea0fe5b7773987ffee269d4913b281c233988f7
Ryan Kleeberger <rylklee@gmail.com>
2017-12-26 22:56:29 -0600

    implemented selection sort

--- DSA.Tests/AlgosTests.cs
+++ DSA.Tests/AlgosTests.cs
@@ -18,8 +18,10 @@ namespace DSA.Tests
             yield return new TestCaseData(new int[] { 1, 0 }).Returns(new int[] { 0, 1 });
             yield return new TestCaseData(new int[] { 1, 0, 2 }).Returns(new int[] { 0, 1, 2 });
             yield return new TestCaseData(new int[] { 5, 2, 1, 0 }).Returns(new int[] { 0, 1, 2, 5 });
-
         }
+
+        [Test, TestCaseSource("UnorderedToOrderedTestCases")]
+        public int[] SelectionSortReturnsSortedArray(int[] arr) => SelectionSort.Run(arr);
 
         [Test, TestCaseSource("UnorderedToOrderedTestCases")]
         public int[] BubbleSortReturnsSortedArray(int[] arr) => BubbleSort.Run(arr);
--- DSA.Tests/SelectionSort.cs
+++ DSA.Tests/SelectionSort.cs
@@ -6,7 +6,24 @@ using System.Threading.Tasks;
 
 namespace DSA.Tests
 {
-    class SelectionSort
+    public static class SelectionSort
     {
+        public static int[] Run(int[] arr)
+        {
+            for (int i = 0; i < arr.Length; i++)
+            {
+                int mark = i;
+                for (int j = i; j < arr.Length; j++)
+                {
+                    if (arr[mark] > arr[j])
+                    {
+                        int temp = arr[j];
+                        arr[j] = arr[mark];
+                        arr[mark] = temp;
+                    }
+                }
+            }
+            return arr;
+        }
     }
 }
