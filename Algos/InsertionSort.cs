8b9269a68a823cf4c54c30016eb230048a1d0511
Ryan Kleeberger <rylklee@gmail.com>
2017-12-28 22:09:10 -0600

    implemented insertion sort

--- Algos/InsertionSort.cs
+++ Algos/InsertionSort.cs
@@ -8,9 +8,23 @@ namespace Algos
 {
     public static class InsertionSort
     {
-        public static int[] Run()
+        public static int[] Run(int[] arr)
         {
+            for (int i = 1; i < arr.Length; i++)
+            {
+                int temp = arr[i]; // store element to be sorted, inserted
+                int shiftMarker = i; // mark element first in line for shifting
 
+                //shift every element up the line until next-in-line is >= temp
+                while (shiftMarker > 0 && arr[shiftMarker - 1] >= temp)
+                {
+                    arr[shiftMarker] = arr[shiftMarker - 1];
+                    shiftMarker--;
+                }
+                
+                arr[shiftMarker] = temp; //insert element into sorted position
+            }
+            return arr;
         }
     }
 }
