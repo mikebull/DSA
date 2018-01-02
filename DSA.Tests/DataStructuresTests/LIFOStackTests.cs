e414057aa2d7f1676b0aa9e290a0e4bdde3a5f83
Ryan Kleeberger <rylklee@gmail.com>
2018-01-02 15:45:21 -0600

    cleaned up using statements

--- DSA.Tests/DataStructuresTests/LIFOStackTests.cs
+++ DSA.Tests/DataStructuresTests/LIFOStackTests.cs
@@ -1,11 +1,6 @@
 ﻿using DSA.DataStructures;
 using NUnit.Framework;
-using NUnit.Framework.Constraints;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DSA.Tests
 {
--- DSA.Tests/ToyExamplesTests/DelimiterMatcherTests.cs
+++ DSA.Tests/ToyExamplesTests/DelimiterMatcherTests.cs
@@ -1,10 +1,5 @@
 ﻿using DSA.ToyExamples;
 using NUnit.Framework;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace DSA.Tests
 {
