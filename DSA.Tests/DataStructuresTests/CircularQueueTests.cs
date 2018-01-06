a406e6ac63d8feb015785857edbf3d36233d5547
Ryan Kleeberger <rylklee@gmail.com>
2018-01-06 01:41:23 -0600

    implemented Circular Queue

--- DSA.Tests/DataStructuresTests/CircularQueueTests.cs
+++ DSA.Tests/DataStructuresTests/CircularQueueTests.cs
@@ -2,40 +2,174 @@
 using NUnit.Framework;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace DSA.Tests.DataStructuresTests
 {
     [TestFixture]
     public class CircularQueueTests
     {
-        private static List<String> elements = new List<String>()
+        private static List<String> _elements = new List<String>()
         {
-            "a", "b", "c", "d", "e", "f", "g", "h", "i", "j"
+            "1", "2", "3", "4", "5", "6", "7", "8", "9", "10"
         };
 
         [Test]
-        public void CanAddString()
+        public void CanEnqueueOneElement()
         {
             CircularQueue Queue = new CircularQueue(10);
-            Queue.Add(elements[0]);
+            Queue.Enqueue(_elements[0]);
         }
 
         [Test]
-        public void CanAddTwoStrings()
+        public void CanEnqueueTwoElements()
         {
             CircularQueue Queue = new CircularQueue(10);
-            Queue.Add(elements[0]);
-            Queue.Add(elements[1]);
+            Queue.Enqueue(_elements[0]);
+            Queue.Enqueue(_elements[1]);
         }
 
         [Test]
-        public void CanPeekAfterOneAdd()
+        public void Enqueue_PeekReturnsFirstEnqueue()
         {
             CircularQueue Queue = new CircularQueue(10);
 
-            Queue.Add(elements[0]);
+            Queue.Enqueue(_elements[0]);
+
             string result = Queue.Peek();
-            Assert.AreEqual(result, elements[0]);
+
+            Assert.AreEqual(_elements[0], result);
+        }
+
+        [Test]
+        public void Enqueue_Enqueue_Peek_ReturnsFirstEnqueue()
+        {
+            CircularQueue Queue = new CircularQueue(10);
+            string firstElement = _elements[0];
+            string secondElement = _elements[1];
+
+            Queue.Enqueue(firstElement);
+            Queue.Enqueue(secondElement);
+
+            string result = Queue.Peek();
+
+            Assert.AreEqual(firstElement, result);
+        }
+
+        [Test]
+        public void AddingMoreThanMaxSizeThrowsException()
+        {
+            CircularQueue Queue = new CircularQueue(10);
+
+            foreach (var element in _elements) Queue.Enqueue(element);
+
+            void a() => Queue.Enqueue("OneTooMany");
+
+            Assert.Throws(typeof(ArgumentException), new TestDelegate(a));
+        }
+
+        [Test]
+        public void Enqueue_Dequeue_ReturnsFirstEnqueue()
+        {
+            CircularQueue Queue = new CircularQueue(10);
+            string firstElement = _elements[0];
+
+            Queue.Enqueue(firstElement);
+            string result = Queue.Dequeue();
+
+            Assert.AreEqual(firstElement, result);
+        }
+
+        [Test]
+        public void Enqueue_Enqueue_Dequeue_Enqueue_Peek_ReturnsSecondEnqueue()
+        {
+            CircularQueue Queue = new CircularQueue(10);
+            string firstElement = _elements[0];
+            string secondElement = _elements[1];
+            string thirdElement = _elements[2];
+
+            Queue.Enqueue(firstElement);
+            Queue.Enqueue(secondElement);
+            Queue.Dequeue();
+            Queue.Enqueue(thirdElement);
+
+            string result = Queue.Peek();
+
+            Assert.AreEqual(secondElement, result);
+        }
+
+        [Test]
+        public void Enqueue_Enqueue_Size_Returns2()
+        {
+            CircularQueue Queue = new CircularQueue(10);
+
+            for (int i = 0; i < 2; i++)
+            {
+                Queue.Enqueue(_elements[i]);
+            }
+
+            int result = Queue.Size();
+
+            Assert.AreEqual(2, result);
+        }
+
+        [Test]
+        public void FillMaxSizeTenQueue_Dequeue_Enqueue_Peek_ReturnsSecondEnqueue()
+        {
+            CircularQueue Queue = new CircularQueue(10);
+
+            foreach (var element in _elements) Queue.Enqueue(element);
+
+            Queue.Dequeue();
+            Queue.Enqueue("a");
+            string result = Queue.Peek();
+
+            Assert.AreEqual("2", result);
+        }
+
+        [Test]
+        public void FillQueue_EmptyQueue_Enqueue_Peek_ReturnsLastEnqueue()
+        {
+            CircularQueue Queue = new CircularQueue(4);
+
+            for (int i = 0; i < 4; i++)
+            {
+                Queue.Enqueue(_elements[i]);
+            }
+
+            for (int i = 0; i < 4; i++)
+            {
+                Queue.Dequeue();
+            }
+
+            string expected = "first after clear";
+            Queue.Enqueue(expected);
+            string result = Queue.Peek();
+            Assert.AreEqual(expected, result);
+        }
+
+        [Test]
+        public void VariousQueue_Enqueue()
+        {
+            CircularQueue Queue = new CircularQueue(4);
+
+            Queue.Enqueue("a");
+            Queue.Enqueue("b");
+            Queue.Enqueue("c");
+            Queue.Enqueue("d");
+            Queue.Dequeue(); //removes a
+            Queue.Dequeue(); //removes b
+            Queue.Dequeue(); //removes c
+            Queue.Enqueue("e");
+            Queue.Enqueue("f");
+            Queue.Enqueue("g");
+            Queue.Dequeue(); //removes d
+            Queue.Enqueue("h");
+            Queue.Dequeue(); //removes e
+            Queue.Dequeue(); //removed f
+            Queue.Dequeue(); //removes g
+            string result = Queue.Peek();
+            Assert.AreEqual("h", result);
         }
     }
 }
--- DSA/DataStructures/CircularQueue.cs
+++ DSA/DataStructures/CircularQueue.cs
@@ -8,17 +8,73 @@ namespace DSA.DataStructures
 {
     public class CircularQueue
     {
-        public string[] Elements { get; set; }
+        private string[] Elements { get; set; }
+        private int MaxSize { get; set; }
+        private int ElementCount { get; set; }
+        private int Front { get; set; }
+        private int Back { get; set; }
 
         public CircularQueue(int size)
         {
-            if (size > 0) Elements = new String[size];
+            if (size > 0)
+            {
+                Elements = new String[size];
+                MaxSize = size;
+                ElementCount = 0;
+                Front = 0;
+                Back = -1;
+            }
             else throw new ArgumentException("Size must be > 0");
         }
 
-        public void Add(string element)
+        public void Enqueue(string element)
         {
-            
+            if (this.Size() == MaxSize)
+            {
+                throw new ArgumentException("Queue is full");
+            }
+            else if (Back == MaxSize - 1)
+            {
+                Back = -1;
+            }
+
+            Elements[Back + 1] = element;
+            Back++;
+            ElementCount++;
         }
+
+        public string Dequeue()
+        {
+            if (this.Size() == 0)
+            {
+                throw new ArgumentException("Queue is empty");
+            }
+
+            string temp = Elements[Front];
+            Elements[Front] = null;
+
+            if (Front == MaxSize - 1)
+            {
+                Front = 0;
+            }
+            else
+            {
+                Front++;
+            }
+
+            ElementCount--;
+            return temp;
+        }
+
+        public string Peek()
+        {
+            return Elements[Front];
+        }
+
+        public int Size()
+        {
+            return ElementCount;
+        }
+
     }
 }
