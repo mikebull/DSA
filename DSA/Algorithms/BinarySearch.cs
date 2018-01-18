7574eb69f5533c300496ca39b8c7552c634f202a
Ryan Kleeberger <rylklee@gmail.com>
2018-01-18 09:32:45 -0600

    spaces for style

--- DSA/Algorithms/BinarySearch.cs
+++ DSA/Algorithms/BinarySearch.cs
@@ -1,17 +1,17 @@
 ﻿namespace DSA.Algorithms
 {
-    //Binary search is a fast, intuitive algorithm that searches for a 
-    //value over a sorted array. The way it works is intuitive and simple.
+    // Binary search is a fast, intuitive algorithm that searches for a 
+    // value over a sorted array. The way it works is intuitive and simple.
     //
-    //  (0) Set floor to first index of array, and ceiling to last
-    //  (1) Guess the midpoint of floor and ceiling
-    //  (2) If the guess is low, raise the floor to index of guess + 1
-    //  (3) If the guess is high, lower the ceiling to index of guess - 1
-    //  (4) Repeat step 1 to 3 until guess is correct or floor > ceiling (nothing left to guess)
+    //   (0) Set floor to first index of array, and ceiling to last
+    //   (1) Guess the midpoint of floor and ceiling
+    //   (2) If the guess is low, raise the floor to index of guess + 1
+    //   (3) If the guess is high, lower the ceiling to index of guess - 1
+    //   (4) Repeat step 1 to 3 until guess is correct or floor > ceiling (nothing left to guess)
     //
-    //The most important thing to note is that this algorithm reduces 
-    //the search space by half every iteration (at step 2 or 3). This makes
-    //it very fast, running in O(log n) time.
+    // The most important thing to note is that this algorithm reduces 
+    // the search space by half every iteration (at step 2 or 3). This makes
+    // it very fast, running in O(log n) time.
 
     public static class MyBinarySearch
     {
