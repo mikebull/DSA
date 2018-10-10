da6cf95ce8e477cda91fa670f8f85b422f0893f5
Ryan Kleeberger <ryanklee@gmail.com>
2018-10-09 19:34:15 -0500

    Incomplete

---
+++ DSA.Tests/AlgorithmsTests/PartitionTests.cs
@@ -0,0 +1,33 @@
+﻿using DSA.Algorithms;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DSA.Tests.AlgorithmsTests
+{
+    [TestFixture]
+    public class PartitionTests
+    {
+
+        [TestCase]
+        public void Part_Correctly_Partitions_Arbitrary_List()
+        {
+            var partition = new Partition();
+
+            var nums = partition.Part(new List<int> { 3, 1, 10, 5, 6, 9, 8, 1, 5, 8, 4 });
+
+            var firstHalf = nums.Take(nums.Count / 2);
+            var partVal = nums[nums.Count / 2];
+            var lastHalf = nums.Skip(nums.Count / 2);
+
+            var allLess = firstHalf.All(num => num <= partVal);
+            var allMore = lastHalf.All(num => num >= partVal);
+
+            Assert.IsTrue(allLess && allMore);
+        }
+
+    }
+}
---
+++ DSA/Algorithms/Partition.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace DSA.Algorithms
+{
+    public class Partition
+    {
+        public int Part(int pivot, List<int> nums)
+        {
+            var leftPtr = 0;
+            var rghtPtr = nums.Count - 1;
+
+            while (true)
+            {
+
+            }
+
+            return 1;
+        }
+
+        private static List<int> Swap(List<int> nums, int j, int i)
+        {
+            var tmp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = tmp;
+            return nums;
+        }
+    }
+}
